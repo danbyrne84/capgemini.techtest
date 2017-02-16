3ab5b9d93d188dd6484e6e2cd9f228d1b12f9694
dan <dan@danielbyrne.net>
2017-02-15 19:57:24 -0800

    Added UI

--- Capgemini.Techtest/Program.cs
+++ Capgemini.Techtest/Program.cs
@@ -10,6 +10,38 @@ namespace Capgemini.Techtest
     {
         static void Main(string[] args)
         {
+            // set up our initial list of menu items
+            var menuItems = new List<MenuItem>()
+            {
+                new MenuItem()
+                {
+                    Name = "cola",
+                    Temperature = Temperature.Cold,
+                    Price = 0.50m
+                },
+                new MenuItem()
+                {
+                    Name = "coffee",
+                    Temperature = Temperature.Hot,
+                    Price = 1.00m
+                },
+                new MenuItem()
+                {
+                    Name = "cheese sandwich",
+                    Temperature = Temperature.Cold,
+                    Price = 2.00m
+                },
+                new MenuItem()
+                {
+                    Name = "steak sandwich",
+                    Temperature = Temperature.Hot,
+                    Price = 4.50m
+                }
+            };
+
+            // initialise the UI
+            var ui = new Ui(new Menu(menuItems), new Bill(), new Console());
+            ui.Go();
         }
     }
 }
--- Capgemini.Techtest/Ui.cs
+++ Capgemini.Techtest/Ui.cs
@@ -10,26 +10,58 @@ namespace Capgemini.Techtest
     {
         private readonly Menu _menu;
         private readonly Bill _bill;
+        private readonly IConsole _console;
 
-        public Ui(Menu menu, Bill bill)
+        public Ui(Menu menu, Bill bill, IConsole console)
         {
+            if (menu == null) { throw new ArgumentNullException("menu"); }
+            if (bill == null) { throw new ArgumentNullException("bill"); }
+            if (console == null) { throw new ArgumentNullException("console"); }
+
             _menu = menu;
             _bill = bill;
+            _console = console;
         }
 
         private string PromptForMenuItem()
         {
-            throw new NotImplementedException();
+            _console.WriteLine("Please enter a menu item, or press enter to complete the bill.");
+            return _console.ReadLine();
         }
 
-        private MenuItem FindMenuItem()
+        public MenuItem TryAddMenuItem(string name)
         {
-            throw new NotImplementedException();
+            var item = _menu.Items.FirstOrDefault(itm => itm.Name.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+            if (item != null)
+            {
+                _bill.AddItem(item);
+            }
+
+            return item;
         }
 
         public void Go()
         {
-            throw new NotImplementedException();
+            _console.WriteLine($"Available menu items are [{string.Join(",", _menu.Items.Select(mi => mi.Name))}]");
+
+            string chosenItem;
+            while ((chosenItem = PromptForMenuItem()) != string.Empty)
+            {
+                // find menu item
+                var item = TryAddMenuItem(chosenItem);
+                if (item == null)
+                {
+                    _console.WriteLine($"Unable to find the menu item {chosenItem}");
+                }
+                else
+                {
+                    _console.WriteLine($"{item.Name} @ {item.Price} added to the bill");
+                }
+            }
+
+            _console.WriteLine($"Thankyou, your bill comes to {_bill.CalculateBill()}");
+            _console.WriteLine("Press any key to end");
+            _console.ReadLine();
         }
     }
 }
