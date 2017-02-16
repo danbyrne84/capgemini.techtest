4d58d475d855c1affb0583516499110a051bb384
dan <dan@danielbyrne.net>
2017-02-15 19:10:16 -0800

    Added failing unit tests for Bill

--- Capgemini.Techtest.UnitTests/Bill.cs
+++ Capgemini.Techtest.UnitTests/Bill.cs
@@ -16,5 +16,43 @@ namespace Capgemini.Techtest.UnitTests
             _underTest = new Capgemini.Techtest.Bill();
             _menu = new Menu(new List<MenuItem>());
         }
+
+        [TestMethod]
+        public void Initialize_BillTotalShouldBeZero()
+        {
+            Assert.AreEqual(0m, _underTest.CalculateBill());
+        }
+
+        [TestMethod]
+        public void CalculateBill_Given_MenuConsistsOfAColaCoffeeAndCheeseSandwich_Assert_BillShouldBe350()
+        {
+            var cola = new MenuItem()
+            {
+                Name = "Cola",
+                Price = 0.50m,
+                Temperature = Temperature.Cold,
+            };
+
+            var coffee = new MenuItem()
+            {
+                Name = "Coffee",
+                Price = 1.00m,
+                Temperature = Temperature.Hot,
+            };
+
+            var cheeseSandwich = new MenuItem()
+            {
+                Name = "Cheese Sandwich",
+                Price = 2.00m,
+                Temperature = Temperature.Cold,
+            };
+
+            _underTest.AddItem(cola);
+            _underTest.AddItem(coffee);
+            _underTest.AddItem(cheeseSandwich);
+
+            Assert.AreEqual(3.50m, _underTest.CalculateBill());
+        }
+
     }
 }
